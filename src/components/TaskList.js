842cdb7306443df111157ecc7d3ed823646a9ae8
Tom Coleman <tom@percolatestudio.com>
2019-03-05 13:28:16 +1100

    Added a Jest test of the ordering of the Updated Composite component for 6.0

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -58,9 +58,13 @@ function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
 }
 
 TaskList.propTypes = {
+  /** Checks if it's in loading state */
   loading: PropTypes.bool,
+   /** The list of tasks */
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
   onPinTask: PropTypes.func.isRequired,
+  /** Event to change the task to archived */
   onArchiveTask: PropTypes.func.isRequired,
 };
 
