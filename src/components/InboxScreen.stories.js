f8c63d2903f89cdf29fe3ff80fb0296b9ea6e87a
Tom Coleman <tom@thesnail.org>
2020-05-18 15:43:18 +1000

    Actually show the designs Storybook 6 updates

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -4,6 +4,13 @@ import { action } from "@storybook/addon-actions";
 import { PureInboxScreen } from "./InboxScreen";
 import * as TaskListStories from './TaskList.stories'
 
+export default {
+  component: PureInboxScreen,
+  title: 'InboxScreen',
+  parameters: { assets: ['designs/app.png'] },
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+};
+
 // A super-simple mock of a redux store
 const store = {
   getState: () => {
@@ -13,12 +20,6 @@ const store = {
   },
   subscribe: () => 0,
   dispatch: action("dispatch"),
-};
-
-export default {
-  component: PureInboxScreen,
-  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
-  title: "InboxScreen",
 };
 
 const Template = (args) => <PureInboxScreen {...args} />;
